f039e2a59a565338545049afe60c4b7690f5c72e
Brieuc LAUNAY <brieuc.launay@gmail.com>
2024-11-07 13:38:41 +0100

    added the '-o' argument (open file)

--- main.py
+++ main.py
@@ -1,21 +1,25 @@
-import os, sys
-from PyQt5.QtWidgets import QApplication
-import program.program_conf
+import argparse
+import sys
 
-# sys.path.insert(0, os.path.join( os.path.dirname(__file__), "..", ".." ))
-# from app import PataShade
+# Note: This import is here to avoid a circular import
+import program.program_conf # noqa: F401
+
+from PyQt5.QtWidgets import QApplication
 from app import PataShade
 
-# from window import Window, WindowSize
-# from glcontext import GLWindow, MGLW, QModernGLWidget
-
 if __name__ == "__main__":
+    parser = argparse.ArgumentParser(prog="PataNode", description="The node-oriented shader manager")
+    parser.add_argument("-o", "--open", metavar="filename")
 
-    # print(QStyleFactory.keys())
+    args = parser.parse_args()
+
     app = QApplication(sys.argv)
     app.setStyle("Fusion")
 
     patanode = PataShade()
     patanode.show()
+
+    if args.open:
+        patanode.openFile(args.open)
 
     sys.exit(app.exec_())
