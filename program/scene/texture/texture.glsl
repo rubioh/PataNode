2e987c8f1057f909d91138f522e5b5c767603730
rcargou <rcargou@hotmail.fr>
2024-11-14 23:11:52 +0100

    introduce texture scene

---
+++ program/scene/texture/texture.glsl
@@ -0,0 +1,13 @@
+#version 330 core
+layout (location=0) out vec4 fragColor;
+
+uniform vec2 iResolution;
+uniform sampler2D iChannel0;
+void main()
+{
+    // Normalized pixel coordinates (from 0 to 1)
+    vec2 uv = ( 2.*gl_FragCoord.xy - iResolution.xy ) / iResolution.y;
+
+    fragColor = vec4(texture(iChannel0, uv).rgba);
+}
+
