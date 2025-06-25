a900b9a80d9c9f98c9989ec37eea62a71de7f1c2
rcargou <rcargou@hotmail.fr>
2025-06-25 23:24:05 +0200

    update triforce

--- program/scene/triforce/triforce.glsl
+++ program/scene/triforce/triforce.glsl
@@ -9,6 +9,16 @@ uniform float bpm;
 uniform float intensity;
 uniform float tf;
 uniform float scale;
+uniform vec3 d11;
+uniform vec3 d12;
+uniform vec3 d13;
+uniform vec3 d21;
+uniform vec3 d22;
+uniform vec3 d23;
+uniform vec3 d31;
+uniform vec3 d32;
+uniform vec3 d33;
+
 
 float line(vec2 uv, vec2 line, float l) {
 
@@ -18,18 +28,17 @@ vec2 pp = uv + line * k;
     smoothstep(0.01, 0.00, length(pp) - l) ;
 }
 
-float triangle(vec2 uv) {
-float sin_60_degree = 0.5;
-float cos_60_degree = 0.86602540378;
+vec3 triangle(vec2 uv, mat3 ddd) {
+    float sin_60_degree = 0.5;
+    float cos_60_degree = 0.86602540378;
 
-float l = .289;
-float d = .145;//l / 1.52;
-float kk = .31;
-       float c = line(uv + vec2(-d, kk), normalize(vec2(cos_60_degree, sin_60_degree)), l );
-    c = max(c, line(uv + vec2(d, kk), normalize(vec2(-cos_60_degree, sin_60_degree)), l ));
-   
-   c = max(c,  line(uv + vec2(.0, .56), normalize(vec2(0., 1.)),l ));
-return c;
+    float l = .289;
+    float d = .145;//l / 1.52;
+    float kk = .31;
+    vec3 c = ddd[1] * line(uv + vec2(-d, kk), normalize(vec2(cos_60_degree, sin_60_degree)), l );
+    c = c +  ddd[0] * line(uv + vec2(d, kk), normalize(vec2(-cos_60_degree, sin_60_degree)), l );   
+    c = c + ddd[2] * line(uv + vec2(.0, .56), normalize(vec2(0., 1.)),l );
+    return c;
 }
 
 void main()
@@ -41,9 +50,9 @@ void main()
 float dd = .295;
 float dy = -.51;
 //float c = triangle(uv);
-     float c = triangle(uv + vec2(-dd,0));
-     c = c + triangle(uv + vec2(dd, 0.));
-     c = c + triangle(uv + vec2(0., dy));
+     vec3 c = triangle(uv + vec2(-dd,0), mat3(d31, d32, d33));
+     c = c + triangle(uv + vec2(dd, 0.), mat3(d11, d12, d13));
+     c = c + triangle(uv + vec2(0., dy), mat3(d21, d22, d23));
  
- fragColor = vec4(c,c,c,1. + 0.1 * (energy_fast + energy_mid + energy_slow + bpm + intensity + tf + scale + iTime));
+ fragColor = vec4(c,1. + 0.1 * (d11 + energy_fast + energy_mid + energy_slow + bpm + intensity + tf + scale + iTime));
 }
--- program/scene/triforce/triforce.py
+++ program/scene/triforce/triforce.py
@@ -4,9 +4,23 @@ from node.node_conf import register_node
 from node.shader_node_base import ShaderNode, Scene
 from program.program_base import ProgramBase
 from program.program_conf import SQUARE_VERT_PATH, register_program, name_to_opcode
-
+import glm
+import keyboard
 
 OP_CODE_TRIFORCE = name_to_opcode("Triforce")
+RED = glm.vec3(1.0, 0.0, 0.0)
+PURPLE = glm.vec3(1.0, 0.0, 1.0)
+CYAN = glm.vec3(0.0, 1.0, 1.0)
+BLUE = glm.vec3(0.0, 0.0, 1.0)
+WHITE = glm.vec3(1.0, 1.0, 1.0)
+BLACK = glm.vec3(0.2, 0.2, 0.2)
+
+PAL1 = glm.vec3(24.0 / 255.0, 1.0 / 255.0, 97.0 / 255.0)
+PAL2 = glm.vec3(79.0 / 255.0, 23.0 / 255.0, 135.0 / 255.0)
+PAL3 = glm.vec3(235.0 / 255.0, 54.0 / 255.0, 120.0 / 255.0)
+PAL4 = glm.vec3(251.0 / 255.0, 119.0 / 255.0, 60.0 / 255.0)
+
+COLOR = [PAL1, PAL2, PAL3, PAL4]
 
 
 @register_program(OP_CODE_TRIFORCE)
@@ -34,20 +48,177 @@ class Triforce(ProgramBase):
         frag_path = join(dirname(__file__), "Triforce.glsl")
         self.loadProgramToCtx(vert_path, frag_path, reload, name="")
 
+    def get_col(self):
+        return COLOR
+
+    def apply_cols(self):
+        self.d11 = self.colors[0]
+        self.d12 = self.colors[1]
+        self.d13 = self.colors[2]
+        self.d21 = self.colors[3]
+        self.d22 = self.colors[4]
+        self.d23 = self.colors[5]
+        self.d31 = self.colors[6]
+        self.d32 = self.colors[7]
+        self.d33 = self.colors[8]
+
     def initParams(self):
-        pass
+        keyboard.on_press_key("n", self.upp)
+        keyboard.on_press_key("p", self.down)
+        self.wo = 0
+        self.colors = [
+            glm.vec3(0.0, 0.0, 0.0),
+            glm.vec3(0.0, 0.0, 0.0),
+            glm.vec3(0.0, 0.0, 0.0),
+            glm.vec3(0.0, 0.0, 0.0),
+            glm.vec3(0.0, 0.0, 0.0),
+            glm.vec3(0.0, 0.0, 0.0),
+            glm.vec3(0.0, 0.0, 0.0),
+            glm.vec3(0.0, 0.0, 0.0),
+            glm.vec3(0.0, 0.0, 0.0),
+        ]
+        self.apply_cols()
+        self.kick = 0
+        self.effect_index = 0
+        self.effects = [
+            self.effect_1,
+            self.effect_3,
+            self.effect_4,
+            self.effect_5,
+            self.effect_6,
+            self.effect_7,
+            self.effect_8,
+            self.effect_9,
+            self.effect_10,
+            self.effect_11,
+            self.effect_12,
+            self.effect_13,
+        ]
 
     def initUniformsBinding(self):
-        binding = {}
+        binding = {
+            "d11": "d11",
+            "d12": "d12",
+            "d13": "d13",
+            "d21": "d21",
+            "d22": "d22",
+            "d23": "d23",
+            "d31": "d31",
+            "d32": "d32",
+            "d33": "d33",
+        }
         super().initUniformsBinding(binding, program_name="")
         super().addProtectedUniforms([])
+
+    def self_whole_triangle(self, id, col):
+        self.colors[id * 3 + 0] = col
+        self.colors[id * 3 + 1] = col
+        self.colors[id * 3 + 2] = col
+
+    def set_all(self, col):
+        self.self_whole_triangle(0, col)
+        self.self_whole_triangle(1, col)
+        self.self_whole_triangle(2, col)
+
+    def exterior(self, col):
+        self.colors[0] = col
+        self.colors[2] = col
+
+        self.colors[0 + 3] = col
+        self.colors[1 + 3] = col
+
+        self.colors[1 + 6] = col
+        self.colors[2 + 6] = col
+
+    def interior(self, col):
+        self.colors[1] = col
+        self.colors[2 + 3] = col
+        self.colors[0 + 6] = col
+
+    def effect_5(self, af=None):
+        self.set_all(BLACK)
+        col = self.get_col()[(self.kick // 3) % 4]
+        if self.kick % 2 == 0:
+            self.exterior(col)
+        else:
+            self.interior(col)
+
+    def effect_13(self, af=None):
+        col = af["decaying_kick"] * self.get_col()[(self.kick) % 4]
+        if self.kick % 2 == 0:
+            self.exterior(col)
+        else:
+            self.interior(col)
+
+    def effect_3(self, af=None):
+        col = self.get_col()[(self.kick // 3) % 4]
+        p = af["decaying_kick"]
+        self.set_all(p * col)
+
+    def effect_6(self, af=None):
+        col = self.get_col()[(self.kick) % 4]
+        p = af["decaying_kick"]
+        self.set_all(p * col)
+
+    def effect_7(self, af=None):
+        for i in range(9):
+            self.colors[i] = self.get_col()[(self.kick + i) % 3]
+
+    def effect_12(self, af=None):
+        for i in range(9):
+            self.colors[i] = af["decaying_kick"] * self.get_col()[(self.kick + i) % 3]
+
+    def effect_8(self, af=None):
+        self.set_all(BLACK)
+        self.interior(self.get_col()[self.kick % 4])
+        self.exterior(self.get_col()[(self.kick + 1) % 4])
+
+    def effect_9(self, af=None):
+        self.self_whole_triangle(0, self.get_col()[(self.kick + 0) % 4])
+        self.self_whole_triangle(1, self.get_col()[(self.kick + 1) % 4])
+        self.self_whole_triangle(2, self.get_col()[(self.kick + 2) % 4])
+
+    def effect_10(self, af=None):
+        self.set_all(BLACK)
+        self.interior(af["decaying_kick"] * self.get_col()[self.kick % 4])
+
+    def effect_11(self, af=None):
+        self.set_all(BLACK)
+        self.exterior(af["decaying_kick"] * self.get_col()[self.kick % 4])
+
+    def effect_1(self, af=None):
+        col = self.get_col()[(self.kick // 3) % 4]
+        self.set_all(BLACK)
+        self.self_whole_triangle(self.kick % 3, col)
+
+    def effect_4(self, af=None):
+        col = self.get_col()[(self.kick) % 4]
+        self.set_all(BLACK)
+        self.self_whole_triangle(self.kick % 3, col)
+
+    def upp(self, b):
+        self.effect_index = self.effect_index + 1
+
+    def down(self, b):
+        self.effect_index = self.effect_index - 1
 
     def updateParams(self, af=None):
         if af is None:
             return
+        if af["on_tempo"] > 0.9 and not self.wo:
+            self.wo = True
+            self.kick = self.kick + 1
+        if af["on_tempo"] <= 0.9:
+            self.wo = False
+        self.effects[self.effect_index % len(self.effects)](af)
+        self.apply_cols()
 
     def norender(self):
         return self.fbos[0].color_attachments[0]
+
+    def bindUniform(self, af):
+        super().bindUniform(af)
+        self.programs_uniforms.bindUniformToProgram(af, program_name="")
 
     def render(self, af=None):
         self.updateParams(af)
