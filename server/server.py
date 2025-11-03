23a046652dd091ffc7249103feefe64a4c0bc0c6
rcargou <rcargou@hotmail.fr>
2025-11-03 17:47:07 +0100

    Add server polling and screen flip

--- server/server.py
+++ server/server.py
@@ -18,7 +18,10 @@ class SimpleHandler(BaseHTTPRequestHandler):
         self.send_response(200)
         self.send_header("Content-type", "application/json")
         self.end_headers()
-        self.wfile.write(json.dumps(self.server.example, indent=4).encode("utf-8"))
+        # print(type(), self.server.graphs_data)
+        json_data = self.server.graphs_data
+        #        json_data = json.loads(json_data)
+        self.wfile.write(json.dumps(json_data).encode("utf-8"))
 
     def do_POST(self):
         content_length = int(self.headers.get("Content-Length", 0))
@@ -31,15 +34,18 @@ class SimpleHandler(BaseHTTPRequestHandler):
 
 
 class PataServer:
-    def __init__(self, args, host="localhost", port=4242):
-        with open("./server/example.json") as f:
-            self.example = json.load(f)
+    def __init__(self, args, app, host="localhost", port=4242):
+        #        with open("./server/example.json") as f:
+        #            self.example = json.load(f)
+        # self.server.example = self.example
+        self.app = app
         self.server = HTTPServer((host, port), SimpleHandler)
-        self.server.example = self.example
+        self.server.graphs_data = {}
         self.thread = threading.Thread(target=self.server.serve_forever)
         self.thread.daemon = True  # Allows program to exit even if thread is running
 
     def update(self):
+        self.server.graphs_data = self.app.poll_graphs()
         time.sleep(1.0 / 60.0)
         pass
 
